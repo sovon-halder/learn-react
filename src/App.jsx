8e2d9f11935cff2c9d5f932183def7138fcb6bfc
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-07-05 23:27:18 +0600

    function

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,18 @@ import Footer from './component/Footer';
 const App = () => {
 
 
-  let markes=55;
+  let markes=90;
 
   return (
     <div>
 
-    {(
-      ()=>{
-            if(markes>90 && markes<100){
-              return(<h1>A+</h1>)
-            }
-            else if(markes>80 && markes<89){
-              return (<h1>A gread</h1>)
-            }
-            else if(markes>70 && markes<79){
-              return 'A-'
-            }
-            else if(markes>60 && markes<69){
-              return "A"
-            }
-            else if(markes>50 && markes<59){
-              return 'B'
-            }
-            
+      {(()=>{
+        if(markes>60){
+          return <h4> perfect</h4>
+        }
       }
-    )()}
+
+      )()}
      
       
      
