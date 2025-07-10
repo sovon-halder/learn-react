85d4896d6cac723a363cace72b3e5fba220af7b5
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-07-10 20:12:42 +0600

    show alert massege using props

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,15 @@ import Header from './component/Header';
 
 const App=()=>{
 
-
-  const item={
-        
-    name:"sovon",
-    age: 21,
-    city:"dhaka",
-    phone:'01866955'
-  }
-
+const btn=()=>{
+  alert("hello devlopers ");
+}
 
 
 
   return(
 <div>
-      <Header itemob={item}/>
+      <Header alertBtn={btn}/>
 </div>
       
 
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 
 const Header = (props) => {
     return (
-        <div>
-            <ul>
-                <li>{props.itemob['name']}</li>
-                <li>{props.itemob['age']}</li>
-                <li>{props.itemob['city']}</li>
-                <li>{props.itemob['phone']}</li>
-            </ul>
-        </div>
+<div>
+    <button onClick={props.alertBtn}>submit</button>
+</div>
     );
 };
 
