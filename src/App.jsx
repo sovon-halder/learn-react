8ee0e916e0ff5da0ee319312b881ce830215ca32
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-07-16 07:22:35 +0600

    pic valus from user

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import { useRef } from "react";
 
 const App=()=>{
  
-  let firstname= useRef();
-  let lastname= useRef();
+ let firstname,lastname= useRef();
+ 
+  
 
   const val=()=>{
-    let fname= firstname.current.value;
-    let lname= lastname.current.value;
+   let fname= firstname.value;
+    let lname= lastname.value;
     alert(fname+ " "+lname)
+    
   }
 
   return(
    <div>
-       <input ref={firstname} placeholder="first name"/><br/>
-       <input ref={lastname} placeholder="last name"/><br/>
-
+       <input ref={(a)=> firstname= a}placeholder="first name"/><br/>
+       <input ref={(b)=> lastname= b}placeholder="last name"/><br/>
        <button onClick={val}>Click</button>
 
    </div>
