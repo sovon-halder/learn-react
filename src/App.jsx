5922a3e8b5ff70e7434af94dec0bf8e394a4f504
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-07-18 22:45:04 +0600

    to convert json format

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,18 @@
  
   const App=()=>{
 
-    const a=()=>{
-      fetch("https://jsonplaceholder.typicode.com/posts/103").then((res)=> {
-        if(!res.ok){
-          const mass=`Error:${res.status}`
-          throw new Error(mass)
-        }
-        return res.json()
-      })
-      .then((res)=>console.log(res))
-      .catch((err)=>console.log(err))
-    }
+  const a=()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then((res)=>res.json())
+    .then((res)=>console.log(res))
+
+  }
 
 
       
   return(
    <div>
-    <h1>hello</h1>
+    <h1>using fetch method to fetching data</h1>
     <button onClick={a}>click</button>
           
    </div>
