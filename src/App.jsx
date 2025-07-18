4dbf782c9e44e9c5c997948ffd310edeeda71c83
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-07-18 22:29:04 +0600

    fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import { useRef } from "react";
 
 
 
-const App=()=>{
 
  
-const number= useRef("initialValue: 0");
+  const App=()=>{
 
-const change=()=>{
-let number= number.current++;
-console.log(number);
-}
- 
-  
+    const a=()=>{
+      fetch("https://jsonplaceholder.typicode.com/posts/103").then((res)=> {
+        if(!res.ok){
+          const mass=`Error:${res.status}`
+          throw new Error(mass)
+        }
+        return res.json()
+      })
+      .then((res)=>console.log(res))
+      .catch((err)=>console.log(err))
+    }
 
-  
 
+      
   return(
    <div>
-       
-      <button onClick={change}>click</button>
-
+    <h1>hello</h1>
+    <button onClick={a}>click</button>
+          
    </div>
   );
 };
