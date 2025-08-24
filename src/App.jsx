280602c068803a245dc2e5569b8ffe7fb9749569
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-08-24 11:11:18 +0600

    class

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@
  
   const App=()=>{ 
 
-    const status=true; 
-    
-    
-  
-    switch(status){
-      case true:
-        return <h1>status is true</h1>
-      case false:
-        return <h1>status is false</h1>
-        default:<h1>status is unknwn</h1>
+ 
+  class car{
+    constructor(name){
+      this.brand=name;
     }
+    present(){
+      return 'I have a '+ this.brand;
+    }
+  }
 
+  const mycar=new car("Ford");
+  mycar.present();
   };
 
 
