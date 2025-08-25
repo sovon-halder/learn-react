beb5fb6fa8896e4bfca6d8db3576d49a92059714
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-08-25 23:14:32 +0600

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@
 
   return (
     <div>
-    {(()=>{
-      if(false){
-        return <h1>Hello World</h1>
-      }else{
-        return <h1>Goodbye World</h1>
-      }
-    })()}
+   {(()=>{
+    if(true){
+      return <h1>welcome</h1>;
+    }else{
+      return <h1>getlost</h1>;
+    }
+   })()}
     </div>
   );
   };
